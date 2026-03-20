d566642a9876adbb049428314248a81d0e77a581
gustaar7 <gustavoryan.fxp@gmail.com>
2026-03-20 17:15:48 -0300

    teste2

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function PageCliente() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:8080/agendamentos", {
+      const res = await fetch("https://barbearia-fawn-beta.vercel.app/agendamentos", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
